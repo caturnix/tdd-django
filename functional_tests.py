b591c96e10fb2c8342fe3f32d9906fe3f0d171f0
Владимир Перепелкин <vvperepelkin@sbn-mac-88.local>
2017-06-29 12:42:33 +0300

    fixed h1 selector

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# user notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		header_text=self.browser.find_elements_by_tag_name('h1').text
+		header_text=self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
 		# user is invited to enter a to-do list straight away
