bb3da09e044dc3201a6db1cda8fc7d3f8ac208e6
Владимир Перепелкин <vvperepelkin@sbn-mac-88.local>
2017-06-30 13:59:04 +0300

    refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table=self.browser.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retieve_it_later(self):
 		# opening homepage
 		self.browser.get('http://localhost:8000')
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table=self.browser.find_element_by_id('id_list_table')
-		rows=table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy feathers', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy feathers')
 
 # there is still textbox inviting to add another item
 # user enters "Use feathers to make a fly"
@@ -53,10 +55,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 # the page updates again and shows both items in a list
 
-		table=self.browser.find_element_by_id('id_list_table')
-		rows=table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy feathers', [row.text for row in rows])
-		self.assertIn('2: Use feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy feathers')
+		self.check_for_row_in_list_table('2: Use feathers to make a fly')
 
 # user sees that the site has generated a unique URL for him
 # there is explanatory text for this effect
