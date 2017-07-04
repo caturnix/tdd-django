203847ae8c44dc7c9ecad3de2a17b06affb59754
Владимир Перепелкин <vvperepelkin@sbn-mac-88.local>
2017-07-04 15:48:24 +0300

    Redirect after POST, db and showing all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -25,8 +25,19 @@ class HomePageTest(TestCase):
 		new_item=Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
+	def test_redirects_after_POST(self):
+		response=self.client.post('/',data={'item_text':'A new list item'})
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], '/')
+
+	def test_displays_all_list_item(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/')
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 	def test_only_saves_items_when_necessary(self):
 		self.client.get('/')
